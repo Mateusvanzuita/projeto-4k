d3980155fa95c4146efd756e6e4b392ff6a06ff9
Mateus Vanzuita <100474707+Mateusvanzuita@users.noreply.github.com>
2026-01-27 14:52:11 -0300

    update

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ self.addEventListener('push', function (event) {
   const data = event.data ? event.data.json() : {};
   const options = {
     body: data.body || 'Você tem uma nova atualização!',
-    icon: '/icons/icon-192x192.png',
-    badge: '/icons/icon-192x192.png',
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
     vibrate: [100, 50, 100],
     data: {
       url: '/aluno/dashboard' 
